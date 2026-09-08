1bcce6e24a2677c72549d645169401c448b9acbc
sunless-dev <matias.franco289@gmail.com>
2026-09-07 21:01:52 -0300

    added connection from the API to the db

---
+++ src/db.ts
@@ -0,0 +1,31 @@
+import { Pool } from "pg";
+
+let connection: Pool | undefined = undefined;
+
+export default async function connectToDB() {
+  if (connection) return connection;
+
+  connection = new Pool({
+    host: process.env.POSTGRES_HOST || "localhost",
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+
+  try {
+    await connection.query("SELECT 1");
+    console.info(`The connection to the DB has been successfully established.`);
+  } catch (err) {
+    console.error(
+      `The following error has occurred while trying to establish a connection to the DB: `,
+    );
+    console.error(err);
+
+    throw new Error(
+      "Unnable to establish connection to the DB. Check logs for more info.",
+    );
+  }
+
+  return connection;
+}
